81e46ccfc2ad3f0271e06ad0668e02bef9508cc4
Hunor <vrshunor@gmail.com>
2025-11-26 18:32:49 +0100

    Új üzenet hozzáadása a feature branch-ben

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps!");
+  res.send("Hello World! Sose gondoltam volna, hogy DEVOPS-ozok, és láss csodát! :)");
 });
 
 app.listen(8080, () => {
